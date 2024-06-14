87eb81f3672976d774741cd90aba8fe9d7f7e05c
Jpedro900 <jpedro.cs09@gmail.com>
2024-06-13 23:35:16 -0300

    Fix: Change typography props

--- src/theme.ts
+++ src/theme.ts
@@ -20,6 +20,31 @@ let theme = createTheme({
     h2: {
       fontSize: "3.2rem",
     },
+    body1: {
+      fontSize: "1.6rem",
+      fontWeight: 'bold',
+    },
+    body2: {
+      fontSize: "1.2rem",
+    },
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: 'h2',
+          h2: 'h2',
+          h3: 'h2',
+          h4: 'h2',
+          h5: 'h2',
+          h6: 'h2',
+          subtitle1: 'h2',
+          subtitle2: 'h2',
+          body1: 'span',
+          body2: 'span',
+        },
+      },
+    },
   },
 });
 
