af4dafc9a7739f0e1c2f99d85b911568a6c8671e
Jpedro900 <jpedro.cs09@gmail.com>
2024-07-26 13:37:07 -0300

    .

--- src/components/FormEmail/FormEmail.tsx
+++ src/components/FormEmail/FormEmail.tsx
@@ -2,39 +2,39 @@ import { styled } from "@mui/material";
 import emailjs from '@emailjs/browser';
 import React, { useState } from 'react';
 
-const StyledButton = () => {
+const StyledForm = styled('form')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '15px',
+    width: '75%',
+    height: '100%',
+    '& input': {
+        width: '100%',
+        border: "none",
+        borderBottom: "1px solid #dedede",
+        padding: '10px',
+    },
+    '& textarea': {
+        width: '100%',
+        height: '200px',
+        border: "1px solid #dedede",
+        padding: '10px',
+    },
+    '& input[type="submit"]': {
+        width: '30%',
+        alignSelf: 'end',
+        background: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        cursor: 'pointer',
+    }
+}));
+
+const FormEmail = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-
-    const StyledForm = styled('form')(({ theme }) => ({
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '15px',
-        width: '75%',
-        height: '100%',
-        '& input': {
-            width: '100%',
-            border: "none",
-            borderBottom: "1px solid #dedede",
-            padding: '10px',
-        },
-        '& textarea': {
-            width: '100%',
-            height: '200px',
-            border: "1px solid #dedede",
-            padding: '10px',
-        },
-        '& input[type="submit"]': {
-            width: '30%',
-            alignSelf: 'end',
-            background: theme.palette.primary.main,
-            color: theme.palette.primary.contrastText,
-            cursor: 'pointer',
-        }
-    }));
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -54,32 +54,30 @@ const StyledButton = () => {
     };
 
     return (
-        <>
-            <StyledForm onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="Name" 
-                    onChange={(e) => setName(e.target.value)} 
-                    value={name}
-                />
-                <input 
-                    type="email" 
-                    placeholder="Email" 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    value={email}
-                />
-                <textarea 
-                    placeholder="Message" 
-                    onChange={(e) => setMessage(e.target.value)} 
-                    value={message}
-                />
-                <input 
-                    type="submit" 
-                    value="Submit" 
-                />
-            </StyledForm>
-        </>
+        <StyledForm onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder="Name" 
+                onChange={(e) => setName(e.target.value)} 
+                value={name}
+            />
+            <input 
+                type="email" 
+                placeholder="Email" 
+                onChange={(e) => setEmail(e.target.value)} 
+                value={email}
+            />
+            <textarea 
+                placeholder="Message" 
+                onChange={(e) => setMessage(e.target.value)} 
+                value={message}
+            />
+            <input 
+                type="submit" 
+                value="Submit" 
+            />
+        </StyledForm>
     );
 };
 
-export default StyledButton;
+export default FormEmail;
--- src/pages/Home/Projects/Projects.tsx
+++ src/pages/Home/Projects/Projects.tsx
@@ -1,16 +1,25 @@
 import { Grid, Typography, styled } from "@mui/material"
 import ProjectBox from "../../../components/ProjectBox/ProjectBox"
 
+import pillpal from '../../../assets/images/pillPal.png'
+
 const Projects = () => {
 
-    const StyledProjectImage = styled('img')(({theme}) => ({
+    const StyledProjectImage = styled('img')(() => ({
       width: '85%',
-      border: `2px solid ${theme.palette.primary.contrastText}`,
     }))
 
     const StyledProjects= styled('div')(({theme}) => ({
         backgroundColor: theme.palette.primary.main,
         padding: '20px',
+    }))
+
+    const StyledLink = styled('a')(({theme}) => ({
+      color: theme.palette.primary.contrastText,
+      textDecoration: 'subline',
+      '&:hover': {
+        color: theme.palette.secondary.main,
+      }
     }))
 
     return (
@@ -21,12 +30,13 @@ const Projects = () => {
               <Grid xs={12} md={5}>
                 <ProjectBox>
                   <Grid container display="flex" flexDirection="column" mb={4}>
-                    <Typography variant="body1" color="primary.contrastText">Project 2</Typography>
-                    <Typography variant="body2" color="primary.contrastText">Jul 2023 - Dez 2023</Typography>
+                    <Typography variant="body1" color="primary.contrastText">PillPal</Typography>
+                    <Typography variant="body2" color="primary.contrastText">Mar 2024 - Jun 2024</Typography>
+                    <Typography variant="body2" color="primary.contrastText" mt={1}>Github: <StyledLink href="https://github.com/Jpedro900/PillPal" target="blank" >https://github.com/Jpedro900/PillPal</StyledLink></Typography>
                   </Grid>
                   <Grid container display="flex" justifyContent="center">
-                    <StyledProjectImage src="https://via.placeholder.com/150" alt="Project 1"/>
-                    <Typography variant="body2" color="primary.contrastText" textAlign="justify" mt={3}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate ab earum adipisci perferendis deserunt? Quae beatae odit minus nostrum commodi. Cumque dolorem incidunt tempora, provident unde, qui vitae ipsa omnis dolore molestias delectus? Consectetur, voluptatibus.</Typography>
+                    <StyledProjectImage src={pillpal} alt="Project 1"/>
+                    <Typography variant="body2" color="primary.contrastText" textAlign="justify" mt={4}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate ab earum adipisci perferendis deserunt? Quae beatae odit minus nostrum commodi. Cumque dolorem incidunt tempora, provident unde, qui vitae ipsa omnis dolore molestias delectus? Consectetur, voluptatibus.</Typography>
                     <Typography variant="body2" color="primary.contrastText" width="100%" textAlign="start" mt={3}>Tecnologies:</Typography>
                   </Grid>
                 </ProjectBox>
@@ -34,12 +44,13 @@ const Projects = () => {
               <Grid xs={12} md={5}>
                 <ProjectBox>
                   <Grid container display="flex" flexDirection="column" mb={4}>
-                    <Typography variant="body1" color="primary.contrastText">Project 2</Typography>
+                    <Typography variant="body1" color="primary.contrastText">Project 1</Typography>
                     <Typography variant="body2" color="primary.contrastText">Jul 2023 - Dez 2023</Typography>
+                    <Typography variant="body2" color="primary.contrastText" mt={1}>Github: <StyledLink href="#" target="blank" ></StyledLink></Typography>
                   </Grid>
                   <Grid container display="flex" justifyContent="center">
-                    <StyledProjectImage src="https://via.placeholder.com/250" alt="Project 1"/>
-                    <Typography variant="body2" color="primary.contrastText" textAlign="justify" mt={3}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate ab earum adipisci perferendis deserunt? Quae beatae odit minus nostrum commodi. Cumque dolorem incidunt tempora, provident unde, qui vitae ipsa omnis dolore molestias delectus? Consectetur, voluptatibus.</Typography>
+                    <StyledProjectImage src="https://via.placeholder.com/150" alt="Project 1"/>
+                    <Typography variant="body2" color="primary.contrastText" textAlign="justify" mt={4}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate ab earum adipisci perferendis deserunt? Quae beatae odit minus nostrum commodi. Cumque dolorem incidunt tempora, provident unde, qui vitae ipsa omnis dolore molestias delectus? Consectetur, voluptatibus.</Typography>
                     <Typography variant="body2" color="primary.contrastText" width="100%" textAlign="start" mt={3}>Tecnologies:</Typography>
                   </Grid>
                 </ProjectBox>
